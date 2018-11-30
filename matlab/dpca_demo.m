797deb7eb69e881e95917a94f22d39d35ed24422
manu-tej <a.manutej@gmail.com>
2018-11-29 23:21:09 -0500

    changed input

--- matlab/dpca_demo.m
+++ matlab/dpca_demo.m
@@ -1,84 +1,86 @@
-%% This section creates toy data.
-%
-% It should be replaced by actual experimental data. The data should be
-% joined in three arrays of the following sizes (for the Romo-like task):
-%
-% trialNum: N x S x D
-% firingRates: N x S x D x T x maxTrialNum
-% firingRatesAverage: N x S x D x T
-%
-% N is the number of neurons
-% S is the number of stimuli conditions (F1 frequencies in Romo's task)
-% D is the number of decisions (D=2)
-% T is the number of time-points (note that all the trials should have the
-% same length in time!)
-%
-% trialNum -- number of trials for each neuron in each S,D condition (is
-% usually different for different conditions and different sessions)
-%
-% firingRates -- all single-trial data together, massive array. Here
-% maxTrialNum is the maximum value in trialNum. E.g. if the number of
-% trials per condition varied between 1 and 20, then maxTrialNum = 20. For
-% the neurons and conditions with less trials, fill remaining entries in
-% firingRates with zeros or nans.
-%
-% firingRatesAverage -- average of firingRates over trials (5th dimension).
-% If the firingRates is filled up with nans, then it's simply
-%    firingRatesAverage = nanmean(firingRates,5)
-% If it's filled up with zeros (as is convenient if it's stored on hard 
-% drive as a sparse matrix), then 
-%    firingRatesAverage = bsxfun(@times, mean(firingRates,5), size(firingRates,5)./trialNum)
-
-clear all
-
-N = 100;%100;   % number of neurons
-T = 20;%20;     % number of time points
-S = 7;%7;       % number of stimuli
+% %% This section creates toy data.
+% %
+% % It should be replaced by actual experimental data. The data should be
+% % joined in three arrays of the following sizes (for the Romo-like task):
+% %
+% % trialNum: N x S x D
+% % firingRates: N x S x D x T x maxTrialNum
+% % firingRatesAverage: N x S x D x T
+% %
+% % N is the number of neurons
+% % S is the number of stimuli conditions (F1 frequencies in Romo's task)
+% % D is the number of decisions (D=2)
+% % T is the number of time-points (note that all the trials should have the
+% % same length in time!)
+% %
+% % trialNum -- number of trials for each neuron in each S,D condition (is
+% % usually different for different conditions and different sessions)
+% %
+% % firingRates -- all single-trial data together, massive array. Here
+% % maxTrialNum is the maximum value in trialNum. E.g. if the number of
+% % trials per condition varied between 1 and 20, then maxTrialNum = 20. For
+% % the neurons and conditions with less trials, fill remaining entries in
+% % firingRates with zeros or nans.
+% %
+% % firingRatesAverage -- average of firingRates over trials (5th dimension).
+% % If the firingRates is filled up with nans, then it's simply
+% %    firingRatesAverage = nanmean(firingRates,5)
+% % If it's filled up with zeros (as is convenient if it's stored on hard 
+% % drive as a sparse matrix), then 
+% %    firingRatesAverage = bsxfun(@times, mean(firingRates,5), size(firingRates,5)./trialNum)
+% 
+% clear all
+% 
+N = 42;%100;   % number of neurons
+T = 4000;%20;     % number of time points
+S = 12;%7;       % number of stimuli
 D = 2;          % number of decisions
-E = 20;%20;     % maximal number of trial repetitions
-
-% generating three components
-time = (1:T) / 10;
-component{1} = bsxfun(@times, ones(1,S,D,T,E), shiftdim(sin(time), -2)) * 20;
-component{2} = bsxfun(@times, ones(1,S,D,T,E), (1:S)-ceil(S/2)) .* bsxfun(@times, ones(1,S,D,T,E), shiftdim(time, -2));
-component{3} = bsxfun(@times, ones(1,S,D,T,E), shiftdim([-1 1],-1)) .* ...
-    bsxfun(@times, ones(1,S,D,T,E), shiftdim([time(1:end/2)*0 time(1:end/2)], -2)) * 6;
-
-% mixing them randomly along non-orthogonal axes
-V = randn(N, length(component));
-V(:,2) = V(:,2) + V(:,1)/3;
-V(:,3) = V(:,3) + V(:,2)/4;
-V = bsxfun(@times, V, 1./sqrt(sum(V.^2)));
-for c = 1:length(component)
-    components(:,c) = component{c}(:,:);
-end
-firingRates = V*components';
-clear component components 
-
-% adding noise and normalizing
-firingRates = firingRates + randn(size(firingRates));
-firingRates = firingRates - min(firingRates(:)) + 10;
-firingRates = reshape(firingRates, [N S D T E]);
-
-% setting random number of repetitions for each neuron and condition
+E = 35;%20;     % maximal number of trial repetitions
+% 
+% % generating three components
+time = (1:4000) / 10;
+% component{1} = bsxfun(@times, ones(1,S,D,T,E), shiftdim(sin(time), -2)) * 20;
+% component{2} = bsxfun(@times, ones(1,S,D,T,E), (1:S)-ceil(S/2)) .* bsxfun(@times, ones(1,S,D,T,E), shiftdim(time, -2));
+% component{3} = bsxfun(@times, ones(1,S,D,T,E), shiftdim([-1 1],-1)) .* ...
+%     bsxfun(@times, ones(1,S,D,T,E), shiftdim([time(1:end/2)*0 time(1:end/2)], -2)) * 6;
+% 
+% % mixing them randomly along non-orthogonal axes
+% V = randn(N, length(component));
+% V(:,2) = V(:,2) + V(:,1)/3;
+% V(:,3) = V(:,3) + V(:,2)/4;
+% V = bsxfun(@times, V, 1./sqrt(sum(V.^2)));
+% for c = 1:length(component)
+%     components(:,c) = component{c}(:,:);
+% end
+% firingRates = V*components';
+% clear component components 
+% 
+% % adding noise and normalizing
+% firingRates = firingRates + randn(size(firingRates));
+% firingRates = firingRates - min(firingRates(:)) + 10;
+% firingRates = reshape(firingRates, [N S D T E]);
+% 
+% % setting random number of repetitions for each neuron and condition
 ifSimultaneousRecording = false;  % change this to simulate simultaneous 
-                                 % recordings (they imply the same number 
-                                 % of trials for each neuron)
-if ~ifSimultaneousRecording
-    trialNum = randi([5 E], [N S D]);
-else
-    trialNum = repmat(randi([5 E], [1 S D]), [N 1 1]);
-end
-
-for n = 1:N
-    for s = 1:S
-        for d = 1:D
-            firingRates(n,s,d,:,trialNum(n,s,d)+1:E) = nan;
-        end
-    end
-end
-
-% computing PSTHs
+%                                  % recordings (they imply the same number 
+%                                  % of trials for each neuron)
+% if ~ifSimultaneousRecording
+%     trialNum = randi([5 E], [N S D]);
+% else
+%     trialNum = repmat(randi([5 E], [1 S D]), [N 1 1]);
+% end
+% 
+% for n = 1:N
+%     for s = 1:S
+%         for d = 1:D
+%             firingRates(n,s,d,:,trialNum(n,s,d)+1:E) = nan;
+%         end
+%     end
+% end
+% 
+% % computing PSTHs
+trialNum = trial_num;
+firingRates = psth_trial;
 firingRatesAverage = nanmean(firingRates, 5);
 
 %% Define parameter grouping
